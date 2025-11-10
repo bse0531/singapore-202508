890b92a1951133451a94b6ca51d364d53d37ab18
bse0531 <bse0531@gmail.com>
2025-11-10 17:55:56 +0900

    Add files via upload

--- map.js
+++ map.js
@@ -1,5 +1,1 @@
-
-const map = L.map('map').setView([1.3521,103.8198],12);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:19,attribution:'© OpenStreetMap'}).addTo(map);
-[{name:'Hotel',coords:[1.2897,103.7865]},{name:'Gardens by the Bay',coords:[1.2816,103.8636]}]
-.forEach(p=>L.marker(p.coords).addTo(map).bindPopup(p.name));
+console.log('map');
