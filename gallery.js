30e12c9c111746f92b26e73eb56c41f6c7d11285
bse0531 <bse0531@gmail.com>
2025-11-10 16:58:50 +0900

    Delete gallery.js

--- gallery.js
+++
@@ -1,4 +0,0 @@
-
-const files=['sample1.jpg','sample2.jpg','sample3.jpg'];
-const grid=document.getElementById('galleryGrid');
-if(grid){ files.forEach(n=>{ const img=document.createElement('img'); img.src='assets/'+n; img.loading='lazy'; grid.appendChild(img); }); }
